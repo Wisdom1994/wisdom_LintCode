1dc6ad4069d13bf5f11776e2bb81410cae246045
jiangdezhi <821555201@qq.com>
2017-09-06 19:01:29 +0800

    (54)difficult-->string-to-integer-ii(转换字符串到整数)

--- src/main/java/com/wisdom/difficult/StringToIntegerII.java
+++ src/main/java/com/wisdom/difficult/StringToIntegerII.java
@@ -11,7 +11,7 @@ package com.wisdom.difficult;
 public class StringToIntegerII {
     public static void main(String[] args) {
         StringToIntegerII stringToIntegerII = new StringToIntegerII();
-        String str = "";
+        String str = "1.0";
         System.out.println(stringToIntegerII.atoi(str));
     }
 
@@ -24,7 +24,51 @@ public class StringToIntegerII {
      * 3.确定字符整数位的数字长度，对过长者进行处理。去最大正值或者最小负值——判断其为11位十进制整数，使用Long进行存储
      * 4，对结果进行处理，正值向下取整，负值向上取整
      */
-    private int atoi(String str) {
-        return 0;
+    public int atoi(String str) {
+        if (null == str || 0 == str.length()) {
+            return 0;
+        }
+        String[] strs = str.replaceAll(" +", " ").split(" ");
+        for(String xx : strs) {
+            if (!"".equals(xx)) {
+                str = xx.replaceAll("[^\\d.-]", "");
+                break;
+            }
+        }
+        if (!str.matches("-?\\d+\\.\\d+") && !str.matches("-?\\d+")) {
+            return 0;
+        }
+        if (str.contains(".")) {
+            String num = str.split("\\.")[0];
+            if (num.length() > 12) {
+                if (num.startsWith("\\-")) {
+                    return -2147483648;
+                } else {
+                    return 2147483647;
+                }
+            }
+            if (num.startsWith("\\-")) {
+                Long longNum = Long.parseLong(num);
+                return longNum < -2147483648L ? -2147483648 : Integer.parseInt(num);
+            } else {
+                Long longnum = Long.parseLong(num);
+                return longnum > 2147183647L ? 2147483647 : Integer.parseInt(num);
+            }
+        } else {
+            if (str.length() > 12) {
+                if (str.startsWith("\\-")) {
+                    return -2147483648;
+                } else {
+                    return 2147483647;
+                }
+            }
+            if (str.startsWith("\\-")) {
+                Long longNum = Long.parseLong(str);
+                return longNum < -2147483648L ? -2147483648 : Integer.parseInt(str);
+            } else {
+                Long longnum = Long.parseLong(str);
+                return longnum > 2147483647L ? 2147483647 : Integer.parseInt(str);
+            }
+        }
     }
 }
